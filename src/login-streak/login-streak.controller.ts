90fd7f889b41f584463d74a1a7239b99701e77ed
erumtw <heartof.effort@gmail.com>
2024-12-16 02:18:20 +0700

    fix: set default start,end time

--- src/login-streak/login-streak.controller.ts
+++ src/login-streak/login-streak.controller.ts
@@ -8,10 +8,12 @@ import {
   Delete,
   Request,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { LoginStreakService } from './login-streak.service';
 import { CreateLoginStreakDto } from './dto/create-login-streak.dto';
 import { UpdateLoginStreakDto } from './dto/update-login-streak.dto';
+import { JwtAuthGuard } from '@/auth/guard/jwt-auth.guard';
 
 @Controller('login-streak')
 export class LoginStreakController {
@@ -20,14 +22,33 @@ export class LoginStreakController {
   @Get(':uid/login-history-stats')
   async getLoginHistoryStats(
     @Param('uid') uid: number,
-    @Query('startDate') startDate: string,
-    @Query('endDate') endDate: string,
+    @Query('startDate') startDate?: string,
+    @Query('endDate') endDate?: string,
   ) {
+    const today = new Date();
+    const dayOfWeek = today.getDay();
+    const start = new Date(today);
+    const daysToMonday = (dayOfWeek + 6) % 7; // Convert Sunday (0) to 6, Monday (1) to 0, etc.
+    start.setDate(today.getDate() - daysToMonday);
+    const end = new Date(start);
+    end.setDate(start.getDate() + 6); // Add 6 days to get to Sunday
+
+    // const startDate
+    const dateStart = startDate ? new Date(startDate) : start;
+    const dateEnd = endDate ? new Date(endDate) : end;
+
     return await this.loginStreakService.getUserLoginHistoryStats(
       uid,
-      new Date(startDate),
-      new Date(endDate),
+      dateStart,
+      dateEnd,
     );
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('update-login')
+  loggedIn(@Request() req) {
+    return this.loginStreakService.updateUserLoginStreak(+req.user.UID);
+    // return req.user;
   }
 
   @Post()
@@ -56,10 +77,5 @@ export class LoginStreakController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.loginStreakService.remove(+id);
-  }
-
-  @Post()
-  loggedIn(@Request() req) {
-    return this.loginStreakService.updateUserLoginStreak(+req.UID);
   }
 }
