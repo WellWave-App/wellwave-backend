e534ad48029c469a017ad26244ac0bad23c0e90e
erumtw <heartof.effort@gmail.com>
2025-01-07 18:18:17 +0700

    fixs: query null handles

--- src/recommendation/controllers/recommendation.controller.ts
+++ src/recommendation/controllers/recommendation.controller.ts
@@ -9,7 +9,11 @@ import {
   Query,
 } from '@nestjs/common';
 import { ArticleRecommendationService } from '../services/article-recommendation.service';
+import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { PaginatedResponse } from '@/response/response.interface';
+import { Article } from '@/.typeorm/entities/article.entity';
 
+@ApiTags('Recommendations')
 @Controller('get-rec')
 export default class RecommendationController {
   constructor(
@@ -17,11 +21,39 @@ export default class RecommendationController {
   ) {}
 
   @Get('/articles')
+  // @ApiQuery({ name: 'uid', type: Number, description: 'User ID' })
+  // @ApiQuery({
+  //   name: 'limit',
+  //   type: Number,
+  //   required: false,
+  //   description: 'Maximum number of articles to return (default: 9)',
+  // })
+  // @ApiQuery({
+  //   name: 'includeRead',
+  //   type: Boolean,
+  //   required: false,
+  //   description:
+  //     'Include user already read articles in the recommendations? (default: false)',
+  // })
+  // // @ApiResponse({
+  // //   status: 200,
+  // //   description: 'List of recommended articles',
+  // //   // type: PaginatedResponse<Article>, // Ensure PaginatedResponse<Article> is defined
+  // // })
+  // // @ApiResponse({ status: 404, description: 'User not found' })
   async getRecommendations(
     @Query('uid') userId: number,
     @Query('limit') limit?: number,
     @Query('includeRead') includeRead?: boolean,
   ) {
+    if (limit !== null) {
+      limit = 9;
+    }
+
+    if (includeRead !== null) {
+      includeRead = false;
+    }
+    
     return this.recommendationsService.getReccomendedArticle(
       userId,
       limit,
