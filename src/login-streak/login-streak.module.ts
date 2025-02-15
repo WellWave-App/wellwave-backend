404f13ac96478aed07dbc1c146c2036251ef724e
erumtw <heartof.effort@gmail.com>
2025-02-16 02:36:10 +0700

    feat: integrate achievement tracking into login streak service

--- src/login-streak/login-streak.module.ts
+++ src/login-streak/login-streak.module.ts
@@ -4,9 +4,13 @@ import { LoginStreakController } from './controllers/login-streak.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoginStreakEntity } from '../.typeorm/entities/login-streak.entity';
 import { LoginHistory } from '../.typeorm/entities/login-history.entity';
+import { AchievementModule } from '@/achievement/achievement.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LoginStreakEntity, LoginHistory])],
+  imports: [
+    TypeOrmModule.forFeature([LoginStreakEntity, LoginHistory]),
+    AchievementModule,
+  ],
   controllers: [LoginStreakController],
   providers: [LoginStreakService],
   exports: [LoginStreakService],
