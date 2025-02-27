fbd658151969f6261dc0d1881937d81ac1c7d82d
erumtw <heartof.effort@gmail.com>
2025-02-27 19:08:27 +0700

    feat: change date columns to timestamp and update quest type naming

--- src/.typeorm/entities/logs.entity.ts
+++ src/.typeorm/entities/logs.entity.ts
@@ -40,7 +40,7 @@ export class LogEntity {
     description: 'Date of the log entry',
     example: '2024-12-22',
   })
-  @PrimaryColumn({ type: 'date' })
+  @PrimaryColumn({ type: 'timestamp' })
   DATE: Date;
 
   @ApiProperty({
